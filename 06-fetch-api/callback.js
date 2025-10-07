95c4fbf1709eb4482a7da9d7ffd47cdc1dd1b2c2
LucasStandridge <lucasmstand02@gmail.com>
2025-10-07 17:44:12 -0400

    fetch api
    fetch api

--- 06-fetch-api/callback.js
+++ 06-fetch-api/callback.js
@@ -4,37 +4,61 @@ function clickEventHappened(e){
     console.log("Clicked!")
 }
 
-//button.addEventListener("click", (e)=>{
-//    console.log("Clicked!")
-//});
-//button.addEventListener("click", e => {console.log("Clicked!")});
+button.addEventListener("click", getAndDisplayMonsterData);
+async function getAndDisplayMonsterData(){
+    try{
+    let monResponse = await fetch('https://mhw-db.com/monsters');
+    let monData = await monResponse.json();
+    let holder = document.querySelector("#monDiv");
+    let innerText = "";
 
-//button.addEventListener("click", e => console.log("Clicked!"));
+    for(let i=0;i<10;i++){
+            let randMonster = monData[Math.floor(Math.random() * monData.length)];
+            let randMonsterHTML = document.getElementById("monP");
+            innerText = randMonster.name;
 
-//let dogRequest = fetch("https://dog.ceo/api/breeds/image/random");
-
-//async function getAndDisplayDogImage(){
-  //  let dogResponse = await fetch("https://dog.ceo/api/breeds/image/random");
-    //let dogData  = await dogResponse.json()
-
-//    let dogImageHTML = document.createElement("img");
-//    dogImageHTML.width = 200;
-//    dogImageHTML.src = dogData.message;
-//    document.querySelector("#dogDiv").appendChild(dogImageHTML)
-//}
-//button.addEventListener("click", getAndDisplayDogImage);
-//for (i = 0; i<10;i++){
-//    getAndDisplayDogImage();
-//}
-
-fetch('https://mhw-db.com/monsters')
-    .then(response => response.json())
-    .then(monsters => {
-        for(i=0;i<10;i++){
-            let randMonster = Math.floor(Math.random() * monsters.length);
-            let randMonsterImageHTML = document.createElement("p");
-            randMonsterImageHTML.src = monsters[randMonster].message;
-            document.querySelector("#monsterDiv").appendChild(randMonsterHTML)
+            for (let key of Object.keys(randMonster)){
+                if(typeof randMonster[key] == "object" && randMonster[key] !== null){
+                    continue
+                }
+                else{
+                    innerText+=`<br>${key}: ${randMonster[key]}`
+                }
+            }
+            randMonsterHTML.innerHTML += innerText;
+            holder.appendChild(randMonsterHTML);
         }
-    })
-    .catch(console.log("Whoops"));
+    }
+        catch (error){
+            console.log("whoops",error);
+        }
+}
+
+async function getYGODeck(){
+    try{
+        let ygoResponse = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php');
+        let ygoData = await ygoResponse.json();
+        ygoData = ygoData.data
+        let tempDeck = [];
+        for(let i = 0; i<=40; i++){
+            let randCard = ygoData[Math.floor(Math.random() * ygoData.length)];
+            tempDeck.push(randCard);
+        }
+        let newDeck = [];
+        const types = ["Monster", "Spell", "Trap"];
+        for (let type of types){
+            for (let card of tempDeck){
+                        if (card.type && card.type.includes(type)) {
+                            newDeck.push(card.name);
+                        }
+                    }
+        }
+        let ygoDiv = document.getElementById("ygoDiv");
+        ygoDiv.innerHTML = newDeck;
+    }
+    catch(error){
+        console.log("whoops",error)
+    }
+}
+
+getYGODeck();
